10e731adb4c4c3560b39d46bbed056619eb15f76
Patrick Byers <Patrick.byers.94@gmail.com>
2016-05-19 14:24:57 +1200

    upadte table creation and populate products table

---
+++ sQL/insertData.sql
@@ -0,0 +1,6 @@
+INSERT INTO products(product_name, product_description, price, imageURL) VALUES 
+('Pear', 'A pear is a fruit', '12.50', 'http://images.costumeexpress.com/mgen/adult-pear-costume-cx-808599.jpg?zm=1600,1600,1,0,0'),
+('Apple', 'An apple is a fruit', '1449.00', 'https://pixabay.com/static/uploads/photo/2013/07/13/11/34/apple-158419_960_720.png'),
+('Banana', 'A banana is a fruit', '0.5', 'https://i.ytimg.com/vi/53al2aBX3E8/maxresdefault.jpg'),
+('Orange', 'Orange you glad i didnt say banana', '2.50', 'https://ebentually.files.wordpress.com/2012/06/eben_orange01.gif?w=696'),
+('Pineapple', 'A pineapple is a fruit', '0.01', 'https://s-media-cache-ak0.pinimg.com/736x/ec/da/cf/ecdacf3dd5411eadfd6b350bcaf98f79.jpg');
