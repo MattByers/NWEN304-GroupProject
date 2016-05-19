6fcd81bc23e1cfd276eb41d47352868f7e1ebeb8
David Newton <newtondavi2@zest.ecs.vuw.ac.nz>
2016-05-19 14:52:31 +1200

    Changed index dir

--- shopping-service/index.js
+++ shopping-service/index.js
@@ -31,7 +31,7 @@ app.use(function(req, res, next) {
   next();
 })
 
-app.use(express.static(__dirname + './index'));
+app.use(express.static(__dirname + '/public'));
 
 //Initial browse page, will query the db for all products.
 app.get('/', function(req, res){
--- shopping-service/public/site.js
+++ shopping-service/public/site.js
@@ -5,4 +5,16 @@ $.ajax({
     type: "GET"
 }).then(redraw, ERROR_LOG);
 
+
+//Create a new task todo
+    $('#searchBox').on('click', '#searchButton', fucntion(){
+    		var searchString = $('#searchText').val();
+        	$.ajax({
+                	url: "/",
+                	type: "GET",
+                	dataType: "json",
+                	data: {sendData: searchString}
+            
+    });});
+
 });
