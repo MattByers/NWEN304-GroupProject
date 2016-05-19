96cc154895e20c4ad2048060cadc0557c0b6f1b3
David Newton <newtondavi2@zest.ecs.vuw.ac.nz>
2016-05-19 13:23:00 +1200

    Basic index.html added

--- shopping-service/index.js
+++ shopping-service/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require('express'); 
+var app = express(); 
+var port = process.env.PORT || 8080; 
+var path = require("path");
+var pg = require("pg").native;
 
 var app = express();
 
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() ); // to support JSON­encoded bodies 
+app.use(bodyParser.urlencoded({ // to support URL­encoded bodies 
+extended: true 
+}));  
+
+app.use(function(req, res, next) {
+    if (req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization')
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE')
+        if (req.method === 'OPTIONS') return res.send(200)
+    }
+    next();
+})
+
+
 app.get('/', function(request, response){
-  response.send("Test");
+  response.sendFile(path.join(__dirname+'/views/index.html'));
 });
 
 app.listen(8080);
