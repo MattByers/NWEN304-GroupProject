675ab78b054c68c658cc0f78b58490cb823eedef
David Newton <newtondavi2@zest.ecs.vuw.ac.nz>
2016-05-19 16:18:42 +1200

    Image prepend

--- shopping-service/public/site.js
+++ shopping-service/public/site.js
@@ -1,31 +1,42 @@
 $(document).ready(function (e) {
 var ERROR_LOG = console.error.bind(console);
+
 $.ajax({
+
     url: "/products",
-    type: "GET"
-}).then(redraw, ERROR_LOG);
+    type: "GET",
+
+    success: function(result){
+          redraw(result.data);
+    }
+
+});
+
 
 
 //Create a new task todo
     $('#searchButton').on('click', function(){
-    		alert("CLICKED");
+
     		var searchString = $('#searchText').val();
         	$.ajax({
                 	url: "/products",
                 	type: "PUT",
                 	dataType: "json",
-                	data: {name: searchString}
+                	data: {name: searchString},
 
                 	success: function(result){
-
                 		redraw(result.data);
+                	},
+                	error: function(result){
+                		alert("This isnt working");
                 	}
 
     });});
 
    function redraw(data){
             var taskHTML = '<img src ="';
-            taskHTML += data[0].imageURL;
+            
+            taskHTML += data[0].imageurl;
             taskHTML += '"/>'
             var $newTask = $(taskHTML);
             $('#display').prepend($newTask);
