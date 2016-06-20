648d511602f6e35aa8c7b2ea2e3c5895e1286a96
patrickbyers94 <patrick.byers.94@gmail.com>
2016-06-20 13:09:18 +1200

    update to sql files, to acheive the new database structure

--- sQL/createTables.sql
+++ sQL/createTables.sql
@@ -1,16 +1,21 @@
 CREATE TABLE users(
 userID serial Primary Key,
 username VARCHAR(64),
-password VARCHAR(64),   /*The password digests will only be 60 chars, this can be changed*/
+password VARCHAR(60),   /*The password digests will only be 60 chars, this can be changed*/
 email VARCHAR(64),
 first_name VARCHAR(32),
 last_name VARCHAR(32),
 address VARCHAR(64)
 );
 
+CREATE TABLE product_types(
+product_type VARCHAR(64) Primary Key);
+
 CREATE TABLE products(
 productID serial Primary Key,
 product_name VARCHAR(64),
 product_description VARCHAR(2048),
-price INT
+product_type VARCHAR(64),
+price INT,
+CONSTANT fk_producttype FOREIGN KEY (product_type) REFERENCES product_type(product_type);
 );
--- sQL/insertData.sql
+++ sQL/insertData.sql
@@ -1,6 +1,16 @@
+INSERT INTO product_type(product_type) VALUES
+('Pear'),('Apple'),('Orange'),('Banana');
+
 INSERT INTO products(product_name, product_description, price, imageURL) VALUES 
-('Pear', 'A pear is a fruit', '12.50', 'http://images.costumeexpress.com/mgen/adult-pear-costume-cx-808599.jpg?zm=1600,1600,1,0,0'),
-('Apple', 'An apple is a fruit', '1449.00', 'https://pixabay.com/static/uploads/photo/2013/07/13/11/34/apple-158419_960_720.png'),
-('Banana', 'A banana is a fruit', '0.5', 'https://i.ytimg.com/vi/53al2aBX3E8/maxresdefault.jpg'),
-('Orange', 'Orange you glad i didnt say banana', '2.50', 'https://ebentually.files.wordpress.com/2012/06/eben_orange01.gif?w=696'),
-('Pineapple', 'A pineapple is a fruit', '0.01', 'https://s-media-cache-ak0.pinimg.com/736x/ec/da/cf/ecdacf3dd5411eadfd6b350bcaf98f79.jpg');
+('Green Anjou', 'Sweetly mellow with a smooth and juicy texture, this is the kind of pear to eat for breakfast', 'Pear', '12.50', 'http://www.saveur.com/sites/saveur.com/files/styles/large_1x_/public/import/2010/images/2010-12/7-TASTING_NOTES-PEARS-ANJOU.jpg?itok=xdqcq3aN'),
+('Red Anjou', 'Similar in size and texture to its green cousin, the Red Anjou is sweeter and milder, with subtle hints of sweet spice replacing the Green Anjous citrus notes', 'Pear' '10.00', 'http://usapears.org/wp-content/uploads/2015/04/red-anjou-pear.jpg'),
+('Bosc', 'Bosc pears have a more firm, dense flesh than other pear varieties, so they are ideal for use in baking, broiling or poaching', ' Pear', '11.00', 'http://usapears.org/wp-content/uploads/2011/12/bosc-pear.jpg'),
+('Braeburn', 'Braeburn apples have a combination of sweet and tart flavour.', 'Apple', '6.00', 'https://upload.wikimedia.org/wikipedia/en/thumb/f/fc/Braeburn2008.jpg/220px-Braeburn2008.jpg'),
+('Granny Smith', ' The fruit has hard, light green skin and a crisp, juicy flesh', 'Apple', '8.00', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Granny_smith_and_cross_section.jpg/220px-Granny_smith_and_cross_section.jpg'),
+('Jazz', 'It is hard and crisp but juicy. The color is flushes of red and maroon over shades of green, yellow and orange', 'Apple', '10.00', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Scifresh_13.01.2013_13-21-39.jpg/1024px-Scifresh_13.01.2013_13-21-39.jpg'),
+('Valencia  Orange', 'The Valencia orange is a sweet orange.', 'Orange', '13.00', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Starr_070730-7915_Citrus_sinensis.jpg/220px-Starr_070730-7915_Citrus_sinensis.jpg'),
+('Blood Orange', 'The blood orange is a variety of orange with crimson, almost-blood-colored flesh.', 'Orange', '16.00', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/BloodOrange.jpg/220px-BloodOrange.jpg'),
+('Navel Orange', 'Navel oranges thicker skin makes them easy to peel, they are less juicy', 'Orange', '15.00', 'http://www.buyfruit.com.au/images/P/Orange_(Navel)__17099.jpg'),
+('Cavendish', 'The fruits of the Cavendish bananas are eaten raw, used in baking, fruit salads, fruit compotes, and to complement foods', 'Banana', '8.00', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Cavendish_Banana_DS.jpg/220px-Cavendish_Banana_DS.jpg'),
+('Burro', 'The Burro banana is occasionally sold under the name chunky banana as it is stubbier and more of a square shape than the common banana', 'Banana', '10.00', 'http://www.specialtyproduce.com/sppics/194.png'),
+('Red', 'Softer and sweeter than the yellow Cavendish varieties, with a slight mango flavor', 'Banana', '14.00', 'http://healthybenefits.info/wp-content/uploads/2014/03/Red-Banana.jpg');
