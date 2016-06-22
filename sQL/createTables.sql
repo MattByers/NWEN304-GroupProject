210570260cedb3cfa7385239205a6b18d32ac670
Patrick Byers <Patrick.byers.94@gmail.com>
2016-06-22 15:33:26 +1200

    update sql files to include cart and order tables

--- sQL/createTables.sql
+++ sQL/createTables.sql
@@ -9,7 +9,8 @@ address VARCHAR(64)
 );
 
 CREATE TABLE product_types(
-product_type VARCHAR(64) Primary Key);
+product_type VARCHAR(64) Primary Key
+);
 
 CREATE TABLE products(
 productID serial Primary Key,
@@ -20,4 +21,22 @@ price DECIMAL(6,2),
 imageURL VARCHAR(256),
 CONSTRAINT fk_producttype FOREIGN KEY (product_type) REFERENCES 
 product_types(product_type)
+);
+
+CREATE TABLE orders(
+userid int,
+productid int,
+quantity int,
+CONSTRAINT pk_order PRIMARY KEY(userid, productid),
+CONSTRAINT fk_userid FOREIGN KEY (userid) REFERENCES users(userid),
+CONSTRAINT fk_productid FOREIGN KEY (productid) REFERENCES products(productid)
+);
+
+CREATE TABLE carts(
+userid int,
+productid int,
+quantity int,
+CONSTRAINT pk_cart PRIMARY KEY(userid, productid),
+CONSTRAINT fk_userid FOREIGN KEY (userid) REFERENCES users(userid),
+CONSTRAINT fk_productid FOREIGN KEY (productid) REFERENCES products(productid)
 );
--- sQL/dropTables.sql
+++ sQL/dropTables.sql
@@ -1,3 +1,5 @@
+DROP TABLE carts;
+DROP TABLE orders;
 DROP TABLE users;
 DROP TABLE products;
 DROP TABLE product_types;
